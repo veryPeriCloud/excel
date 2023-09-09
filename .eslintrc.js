1f01542a5e170aed1aa8e34973b3eef95f1497db
Oyuna <101694939+veryPeriCloud@users.noreply.github.com>
2023-09-09 15:06:52 +0300

    Framework start (#2)
    * create framework,create table-component
    
    * fix createTable function
    
    * Finish base framework functional

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,8 @@ module.exports = {
   rules: {
     'semi': 'off',
     'comma-dangle': 'off',
-    'require-jsdoc': 'off'
+    'require-jsdoc': 'off',
+    'linebreak-style': 0,
+    'operator-linebreak': 'off'
   }
 }
